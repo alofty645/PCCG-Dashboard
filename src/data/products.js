7cdb60051ac75a6f3676d52a3ce6bb146cebdc2e
Aden <aden.lofthouse@gmail.com>
2021-03-22 11:40:52 +1100

    First commit

---
+++ src/data/products.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        "key": 1,
+        "Model": "SS-63003",
+        "Description": "SteelSerires QcK Large Mousepad",
+        "Barcode": "5707119001762",
+        "Bay": "13O",
+        "Qty": "10",
+    },
+    {
+        "key": 1,
+        "Model": "SS-63004",
+        "Description": "SteelSerires QcK Medium Mousepad",
+        "Barcode": "5707119001779",
+        "Bay": "13O",
+        "Qty": "33",
+    },
+    {
+        "key": 1,
+        "Model": "SS-63008",
+        "Description": "SteelSerires QcK Heavy Mousepad",
+        "Barcode": "5707119001816",
+        "Bay": "13O",
+        "Qty": "-3",
+    }
+]
